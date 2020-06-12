20c8ea6d05aeb096c0a257c1158e4ea8eae98c0e
kiku ishimoto <kikuish@gmail.com>
2020-06-12 04:55:00 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,8 +1,7 @@
-class CreateCharacters < ActiveRecord::Migration[5.2]
+class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
     create_table :characters do |t|
       t.string :name
-      t.integer :actor_id
       t.integer :show_id
     end
   end
