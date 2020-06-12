33268e021cc15547226b87d0a593943a7cbeabfd
kiku ishimoto <kikuish@gmail.com>
2020-06-12 04:54:56 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,5 +1,5 @@
 
-class AddCatchphraseToCharacters < ActiveRecord::Migration[5.2]
+class AddCatchphraseToCharacters < ActiveRecord::Migration[5.1]
 #define a change method in which to do the migration
   def change
     add_column :characters, :catchphrase, :string
